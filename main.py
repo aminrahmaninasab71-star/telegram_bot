0fa1795b495b3b4a1ae7533f6efcf177b4fdb017
aminrahmaninasab71-star <aminrahmaninasab71@gmail.com>
2025-11-30 17:42:48 +0330

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = 8574312533:AAGx7YmE9U7t0ZWPGw8jlm7Jmwa8UxuGJ-o
+TOKEN = "8574312533:AAGx7YmE9U7t0ZWPGw8jlm7Jmwa8UxuGJ-o"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات روی Render فعال شد ✔️")
@@ -10,3 +10,4 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
+
